592e94739269ddb89494238e538ed3fe93d7f30a
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-24 20:50:57 +0300

    JSON WebToken Authentication part 1

--- src/test/java/com/hassialis/philip/broker/watchlist/WatchListControllerTest.java
+++ src/test/java/com/hassialis/philip/broker/watchlist/WatchListControllerTest.java
@@ -10,11 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hassialis.philip.auth.jwt.AuthenticationProviderUserPassword;
 import com.hassialis.philip.broker.data.InMemoryAccountStore;
 import com.hassialis.philip.broker.model.Symbol;
 
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.security.authentication.UsernamePasswordCredentials;
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 
@@ -27,7 +30,7 @@ class WatchListControllerTest {
   private static final UUID TEST_ACCOUNT_ID = WatchListController.ACCOUNT_ID;
 
   @Inject
-  @Client("/account/watchlist")
+  @Client("/")
   HttpClient client;
 
   @Inject
@@ -45,17 +48,36 @@ class WatchListControllerTest {
 
   @Test
   void returnsEmptyWatchListForTestAccount() {
-    final WatchList result = client.toBlocking().retrieve("/", WatchList.class);
+
+    final BearerAccessRefreshToken token = givenUserIsLoggedIn();
+
+    var request = HttpRequest.GET("/account/watchlist").accept(MediaType.APPLICATION_JSON)
+        .bearerAuth(token.getAccessToken());
+
+    final WatchList result = client.toBlocking().retrieve(request, WatchList.class);
     assertNull(result.symbols());
     assertTrue(inMemoryAccountStore.getWatchList(TEST_ACCOUNT_ID).symbols().isEmpty());
     LOG.debug("returnsEmptyWatchListForTestAccount {}", result);
+  }
+
+  private BearerAccessRefreshToken givenUserIsLoggedIn() {
+    final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
+        AuthenticationProviderUserPassword.USERNAME, AuthenticationProviderUserPassword.PASSWORD);
+    var login = HttpRequest.POST("/login", credentials);
+    var response = client.toBlocking().exchange(login, BearerAccessRefreshToken.class);
+    assertEquals(HttpStatus.OK, response.getStatus());
+    final BearerAccessRefreshToken token = response.body();
+    assertNotNull(token);
+    assertEquals(AuthenticationProviderUserPassword.USERNAME, token.getUsername());
+    LOG.debug("Token {} expires in {}", token.getAccessToken(), token.getExpiresIn());
+    return token;
   }
 
   @Test
   void returnsWatchListForTestAccount() {
     givenWatchListForAccountExists();
 
-    var response = client.toBlocking().exchange("/", JsonNode.class);
+    var response = client.toBlocking().exchange("/account/watchlist", JsonNode.class);
     assertEquals(HttpStatus.OK, response.getStatus());
     assertEquals("{\"symbols\":[{\"value\":\"AAPL\"},{\"value\":\"MSFT\"},{\"value\":\"GOOG\"}]}",
         response.getBody().get().toString());
@@ -64,7 +86,7 @@ class WatchListControllerTest {
   @Test
   void canUpdateWatchListForTestAccount() {
     var symbols = Stream.of("AAPL", "MSFT", "GOOG").map(Symbol::new).toList();
-    var request = HttpRequest.PUT("/", new WatchList(symbols)).accept(MediaType.APPLICATION_JSON);
+    var request = HttpRequest.PUT("/account/watchlist", new WatchList(symbols)).accept(MediaType.APPLICATION_JSON);
     final HttpResponse<Object> response = client.toBlocking().exchange(request);
     assertEquals(HttpStatus.OK, response.getStatus());
     assertEquals(symbols, inMemoryAccountStore.getWatchList(TEST_ACCOUNT_ID).symbols());
@@ -76,7 +98,7 @@ class WatchListControllerTest {
     givenWatchListForAccountExists();
     assertFalse(inMemoryAccountStore.getWatchList(TEST_ACCOUNT_ID).symbols().isEmpty());
 
-    var response = client.toBlocking().exchange(HttpRequest.DELETE("/"));
+    var response = client.toBlocking().exchange(HttpRequest.DELETE("/account/watchlist"));
     assertEquals(HttpStatus.NO_CONTENT, response.getStatus());
   }
 
